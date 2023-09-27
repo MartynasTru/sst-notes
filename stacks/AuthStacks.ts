03077d5f99aae6a8f1bd7bb530ea44801b6f6ffa
Martynas <mtru@Martynass-MacBook-Pro.local>
2023-09-27 17:55:53 +0300

    Added layer of Authentication to use API

---
+++ stacks/AuthStacks.ts
@@ -0,0 +1,38 @@
+import { ApiStack } from "./ApiStack";
+import * as iam from "aws-cdk-lib/aws-iam";
+import { StorageStack } from "./StorageStack";
+import { Cognito, StackContext, use } from "sst/constructs";
+
+export function AuthStack({ stack, app}: StackContext){
+    const { api } = use(ApiStack);
+    const { bucket } = use(StorageStack);
+
+    //Create a Cognito User Pool and Identity Pool
+    const auth = new Cognito(stack, "Auth", {
+        login: ["email"],
+    });
+
+    auth.attachPermissionsForAuthUsers (stack, [
+        // Allow access to the API
+        api,
+        // Policy granting access to a specific folder in the S3 bucket
+        new iam.PolicyStatement({
+            actions: ["s3:*"],
+            effect: iam.Effect.ALLOW,
+            resources: [
+                bucket.bucketArn + "/private/${cognito-identity.amazonaws.com:sub}/*",
+            ],
+        }),
+    ]);
+    
+    stack.addOutputs({
+        Region: stack.region,
+        UserPoolId: auth.userPoolId,
+        UserPoolClientId: auth.userPoolClientId,
+        IdentityPoolId: auth.cognitoIdentityPoolId,
+    });
+
+    return {
+        auth,
+    };
+}
